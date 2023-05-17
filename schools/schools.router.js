5cc238703bc55bf7bb69d014a79f6ac5d41bc02a
Siddharth Roychoudhury <debu2in@gmail.com>
2023-05-17 21:27:05 +0200

    added update function

--- schools/schools.router.js
+++ schools/schools.router.js
@@ -34,8 +34,21 @@ schoolsRouter.post("/", async(req,res)=>{
     }
 })
 
-schoolsRouter.put("/:id", (req,res)=>{
-
+schoolsRouter.put("/:id", async(req,res)=>{
+let message = "", status;
+    try{
+        const connection = await connect()
+        const school_id = req.params.id;
+        const schoolsData = await connection.getRepository(schoolsEntity).create(req.body)
+        const results = await connection.getRepository(schoolsEntity).update({...schoolsData},{id:school_id})
+        status = 200;
+        message = results;
+    }catch(e){
+        status = 500;
+        message = e.message;
+    }finally{
+        res.status(status).send(message)
+    }
 
     
 })
